20a54fdee3762610999f82c2d88d7c52cb6cef61
Ashish Rawat <ashish.rawat@ashishr340.successivetech.com>
2022-04-28 20:16:45 +0530

    work with provisoners, loops and dynamic_blocks

---
+++ terraform/dynamic-blocks/main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  region     = "ap-south-1"
+  access_key = "AKIA2HZLLBFST3X2MFJC"
+  secret_key = "8B7zsoigvqdXXDWhDCuGve9SLncQXn4npbrxzjyQ"
+}
+
+
+resource "aws_instance" "ec2_example" {
+
+   ami           = "ami-08ee6644906ff4d6c"
+   instance_type =  "t2.micro"
+   tags = {
+        Name = "Terraform EC2"
+    }
+ 
+
+locals {
+   ingress_rules = [{
+      port        = 443
+      description = "Ingress rules for port 443"
+   },
+   {
+      port        = 80
+      description = "Ingree rules for port 80"
+   }]
+}
+
+resource "aws_security_group" "main" {
+   name   = "resource_with_dynamic_block"
+   vpc_id = data.aws_vpc.main.id
+
+   dynamic "ingress" {
+      for_each = local.ingress_rules
+
+      content {
+         description = ingress.value.description
+         from_port   = ingress.value.port
+         to_port     = ingress.value.port
+         protocol    = "tcp"
+         cidr_blocks = ["0.0.0.0/0"]
+      }
+   }
+
+   tags = {
+      Name = "AWS security group dynamic block"
+   }
+}
