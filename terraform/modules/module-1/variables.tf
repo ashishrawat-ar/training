76b4385f0019b67cce8f142e7f071c47fdf565a0
Ashish Rawat <ashish.rawat@ashishr340.successivetech.com>
2022-04-29 19:54:24 +0530

    work with modules and workspaces

---
+++ terraform/modules/module-1/variables.tf
@@ -0,0 +1,2 @@
+variable "web_instance_type" {default = "t2.micro"}
+variable "ami_id" {default = "ami-08ee6644906ff4d6c"}
---
+++ terraform/modules/variables.tf
@@ -0,0 +1,4 @@
+variable "web_region" {default = "ap-south-1"}
+variable "access_key" {default = "AKIA2HZLLBFST3X2MFJC"}
+variable "secret_key" {default = "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"}
+
